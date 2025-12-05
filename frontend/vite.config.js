1d1f0c9921b2cdb7a9a333a25339b72464223d47
VirjeshChoudhary <virjeshchoudhary6@gmail.com>
2025-12-06 00:51:19 +0530

    add

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,10 +7,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   server:{
-    headers:{
-       "Cross-Origin-Embedder-Policy": "require-corp",
-      "Cross-Origin-Opener-Policy": "same-origin"
-    },
+    // headers:{
+    //    "Cross-Origin-Embedder-Policy": "require-corp",
+    //   "Cross-Origin-Opener-Policy": "same-origin"
+    // },
     proxy:{
       '/cdn':{
         target: 'https://cdn.jsdelivr.net/npm',
