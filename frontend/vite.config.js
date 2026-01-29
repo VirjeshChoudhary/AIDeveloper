0704676124943825b7eef43a3b322bec8e1bb4f9
VirjeshChoudhary <virjeshchoudhary6@gmail.com>
2026-01-30 01:22:20 +0530

    hi

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'build',  // default is 'dist'
+  },
   plugins: [react(),tailwindcss()],
   server:{
     // headers:{
